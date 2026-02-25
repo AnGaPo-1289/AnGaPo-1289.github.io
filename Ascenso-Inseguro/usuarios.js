b887c383d4a4788fd77855036bdc7223aab66cd2
Angel  García <52895662+AnGaPo-1289@users.noreply.github.com>
2026-02-25 04:23:55 -0600

    Add files via upload

---
+++ Ascenso-Inseguro/usuarios.js
@@ -0,0 +1,9 @@
+const usuarios = [
+  { usuario: "angel", password: "C9548673" },
+  { usuario: "chaires", password: "ABC123" }
+];
+
+// Función para validar usuario
+function validarUsuario(user, pass) {
+  return usuarios.find(u => u.usuario === user && u.password === pass);
+}
