d38bb10829a2e2b0feddfb17343a9a3607e1f93a
Angel  García <52895662+AnGaPo-1289@users.noreply.github.com>
2026-02-26 13:34:54 -0600

    Update usuarios.js

--- Ascenso-Inseguro/usuarios.js
+++ Ascenso-Inseguro/usuarios.js
@@ -2,6 +2,7 @@ const usuarios = [
   { usuario: "angel", password: "C9548673" },
    { usuario: "Ninya", password: "A10047513" },
   { usuario: "Claux077", password: "A10044529" },
+  { usuario: "zenirmrte5221", password: "D3248452" },
   { usuario: "AriadnaMartinez-g517d", password: "A10101226" },
   { usuario: "chaires", password: "ABC123" }
 ];
