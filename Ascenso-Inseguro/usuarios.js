51add807bb100b31e0441e25679cebe67a01c9b7
Angel  García <52895662+AnGaPo-1289@users.noreply.github.com>
2026-03-05 11:01:55 -0600

    Add new user 'odorupiero9716' to usuarios.js

--- Ascenso-Inseguro/usuarios.js
+++ Ascenso-Inseguro/usuarios.js
@@ -3,6 +3,7 @@ const usuarios = [
    { usuario: "Ninya", password: "A10047513" },
   { usuario: "Claux077", password: "A10044529" },
   { usuario: "zenirmrte5221", password: "D3248452" },
+  { usuario: "odorupiero9716", password: "D0565211" },
   { usuario: "AriadnaMartinez-g517d", password: "A10101226" },
   { usuario: "Martin_cortes", password: "D1159565" },
   { usuario: "chaires", password: "ABC123" }
