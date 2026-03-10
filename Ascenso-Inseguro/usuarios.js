c0a3759b4133ad71627814eacb149f2d7bbd5d50
Angel  García <52895662+AnGaPo-1289@users.noreply.github.com>
2026-03-09 22:19:45 -0600

    Change password for user 'chaires'
    Updated password for user 'chaires' from 'ABC123' to '123'.

--- Ascenso-Inseguro/usuarios.js
+++ Ascenso-Inseguro/usuarios.js
@@ -9,7 +9,7 @@ const usuarios = [
   { usuario: "Martin_cortes", password: "D1159565" },
   { usuario: "edelangel", password: "Aege2025" },
   { usuario: "Mary_Gomez", password: "A10050481" },
-  { usuario: "chaires", password: "ABC123" }
+  { usuario: "chaires", password: "123" }
 ];
 
 // Función para validar usuario
